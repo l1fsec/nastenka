ce45ddd70450b0100a279524c25bb4737703c1d3
l1fsec <54239712+l1fsec@users.noreply.github.com>
2020-11-04 12:59:52 +0100

    Aktualizováno na bootstrap
    převedeno na bootstrap

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="cs">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
     <style>
-        body {
-            text-align: center;
+        .box {
+            padding: 60px;
+            margin: 60px 0;
         }
     </style>
+
+    <title>Hello, world!</title>
+    <a href="pridat.html">Přidat úkol/test!</a>
 </head>
 
-<body>
-    <h1> Nástěnka </h1>
+<body class="box">
+    <h1 class="text-center text-dark">Nástěnka</h1>
 
-    <a href="pridat.html">Přidat úkol/test</a>
     <?php
     $servername = "localhost";
     $username = "root";
@@ -33,14 +37,36 @@
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo "Datum zadání: " . $row["datum_zad"] . "<br>";
-            echo "Text zadání: " . $row["text_zad"] . "<br>";
-            echo "Datum odevzdání: " . $row["datum_ode"] . "<br>";
+
+            echo "<div class='container'>";
+            echo "<div class='row justify-content-md-center'>";
+            echo "<div class='col col-lg-2'>" . $row["datum_zad"] . "</div>";
+            echo "<hr>";
+            echo "<div class='col-md-auto'>" . $row["text_zad"] . "</div>";
+            echo "<hr>";
+            echo "<div class='col col-lg-2'>" . $row["datum_ode"] . "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
         }
     } else {
         echo "0 výsledků";
     }
     $conn->close();
     ?>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
 
 </html>
