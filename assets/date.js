f013e830d9af54e04ac8461caebb0e47a18aacc0
l1fsec <54239712+l1fsec@users.noreply.github.com>
2020-11-06 11:22:18 +0100

    date.js přidáno

--- assets/date.js
+++ assets/date.js
@@ -1,6 +1,6 @@
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, '0');
- var mm = String(today.getMonth() + 1).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
   var yyyy = today.getFullYear();
   today = yyyy + '/' + mm + '/' + dd;
- document.getElementById('datum').innerHTML = today;
+  document.getElementById('datum').innerHTML = today;
