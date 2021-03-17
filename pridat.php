a3767f0fe3c2950142cc9ac9d771693456a37cce
jankubatt <jankubat.1@seznam.cz>
2021-03-17 12:15:40 +0100

    pridani predmetu, format datumu

--- pridat.php
+++ pridat.php
@@ -2,13 +2,12 @@
 header('Location: index.php');
 include('config.php');
 
-$text_zad = "";
-
 $datum_zad = $_POST["datum_zad"];
 $text_zad = xssochrana($_POST["text_zad"]);
 $datum_ode = $_POST["datum_ode"];
+$predmet = $_POST["predmet"];
 
-$sql = "INSERT INTO info (datum_zad, text_zad, datum_ode ) VALUES ('$datum_zad', '$text_zad', '$datum_ode')";
+$sql = "INSERT INTO info (datum_zad, text_zad, datum_ode, predmet) VALUES ('$datum_zad', '$text_zad', '$datum_ode', '$predmet')";
 $conn->query($sql);
 
 function xssochrana($data) {
